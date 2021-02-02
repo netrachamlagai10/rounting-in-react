ef746f362c59ec8300c92ded5c5c6f4245144c54
Netra Chamlagai <nayan@Netras-MacBook-Pro.local>
2021-02-02 00:19:42 -0500

    React file Add

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route,Switch } from 'react-router-dom';
+//import { Route, Switch} from 'react-router';
+
+
+import './App.css';
+import Welcome from './components/welcome/Welcome';
+import Clock from './components/clock/Clock';
+import Contact from './components/contact/Contact';
+import Navigation from './components/navigation/Navigation';
+import NotFound from './components/notFound/NotFound';
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Switch>
+      <Route 
+      exact path="/" 
+      render={(props)=> <Welcome{...props} name="Netra Chamlagai"/>} 
+      />
+     <Route path="/clock" component= {Clock} />
+     <Route path="/contact" component= {Contact} />
+     <Route path="/welcome/:name" component= {Welcome} />
+
+     <Route>
+       <NotFound/>
+     </Route>
+      </Switch> 
+     
+    </div>
+  );
+}
+
+export default App;
